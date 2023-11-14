64694e511b1213b537e66fac73e16bea9710fd88
rinrinhye <115080424+rinrinhye@users.noreply.github.com>
2023-11-15 04:24:31 +0900

    header 스크롤 이벤트 추가, lodash throttled 추가

--- js/header.js
+++ js/header.js
@@ -1,11 +1,23 @@
 const header = document.querySelector(".header");
 
-function showHeader() {
-  if (window.scrollY > 100) {
-    header.classList.add("is-scrolled");
-  } else {
+let lastScroll = window.scrollY;
+
+function showAndHideHeader() {
+  let currentScroll = window.scrollY;
+
+  if (lastScroll === 0 || lastScroll === currentScroll) {
+    console.log("로드 or 새로고침");
     header.classList.remove("is-scrolled");
+    lastScroll = currentScroll;
+  } else if (currentScroll - lastScroll > 0) {
+    console.log("내린다");
+    header.classList.add("is-scrolled");
+    lastScroll = currentScroll;
+  } else if (currentScroll - lastScroll < 0) {
+    console.log("올린다");
+    header.classList.remove("is-scrolled");
+    lastScroll = currentScroll;
   }
 }
 
-window.addEventListener("scroll", showHeader);
+window.addEventListener("scroll", _.throttle(showAndHideHeader, 400));
