5de6081efeb68b85a0f7e3494dc02352fe703397
rinrinhye <115080424+rinrinhye@users.noreply.github.com>
2023-11-20 18:15:38 +0900

    console.log 삭제

--- js/header.js
+++ js/header.js
@@ -6,15 +6,12 @@ function showAndHideHeader() {
   let currentScroll = window.scrollY;
 
   if (lastScroll === 0 || lastScroll === currentScroll) {
-    console.log("로드 or 새로고침");
     header.classList.remove("is-scrolled");
     lastScroll = currentScroll;
   } else if (currentScroll - lastScroll > 0) {
-    console.log("내린다");
     header.classList.add("is-scrolled");
     lastScroll = currentScroll;
   } else if (currentScroll - lastScroll < 0) {
-    console.log("올린다");
     header.classList.remove("is-scrolled");
     lastScroll = currentScroll;
   }
